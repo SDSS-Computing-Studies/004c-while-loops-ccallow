43f62c854bc8eef186abbe2a12d8eaac61576768
ccallow <71406811+ccallow@users.noreply.github.com>
2020-09-29 12:41:45 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -16,3 +16,16 @@ Example:
 10
 ...
 """
+import time
+import random
+
+count = 0
+print("========================")
+print("Let's count to 20 by 2s!")
+print("========================")
+while True:
+    count = count + 2
+    n =str(count)
+    print(n)
+    if count == 20:
+        break

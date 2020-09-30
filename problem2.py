51f1337048cbf6dff2c102bf2d283b9b7407517b
ccallow <71406811+ccallow@users.noreply.github.com>
2020-09-30 12:37:46 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -15,7 +15,8 @@ example:
 Enter a number: 4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
-joe = 1
-while joe<=12:
-    a=float(input("int number:  "))
-    
+count = 1
+a=int(input("int number:  "))
+while count<=12:
+    print(a*count, end=" ")
+    count +=1
